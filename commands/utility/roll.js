ec4172fb1639d8e46e3d95fda2ced3c2679834af
Nicole Benes <110053946+nicole-benes@users.noreply.github.com>
2024-04-16 01:58:13 -0400

    Working on roll

--- commands/utility/roll.js
+++ commands/utility/roll.js
@@ -13,13 +13,19 @@ module.exports = {
                     .setRequired( true )
 				.setDescription( 'The number of dice to roll (X) and keep (Y).' ) ),
      async execute(interaction) {
-          const { X: roll, Y: keep, Z: sims } = parseInput( interaction.options.getString( 'roll' ) );
+          const { X: roll, Y: keep, Q: add, Z: sims , e: emphasis, x: expanded } = parseInput( interaction.options.getString( 'roll' ) );
 
           if( sims === null ) {
   
-              const { keptRolls: keptRolls, unkeptRolls: unkeptRolls, total: total } = rollAndKeep( roll, keep );
+              const { keptRolls: keptRolls, unkeptRolls: unkeptRolls, total: total } = rollAndKeep( roll, keep, emphasis, expanded );
   
-              let output = 'Rolled **' + roll + '**k**' + keep + '**:   *Kept Dice*: [ ';
+              let output = 'Rolled **' + roll + '**k**' + keep + '**';
+              
+            if( add > 0 ) {
+                output += '+**' + add + '**';
+            }
+
+              output += ':   *Kept Dice*: [ ';
   
               for( let i = 0; i < keptRolls.length; i++ ) {
   
@@ -47,58 +53,66 @@ module.exports = {
                   output += ' ]';
       
               }
-  
-              output += '   Total: ***' + total + '***';
+
+              output += '   Total: ***' + ( total + add ) + '*** (*' + total + ' + ' + add + '*)';
   
               await interaction.reply( output );               
   
           } else {
   
-              await interaction.deferReply();
-  
-              let simulatedTotal = 0;
-  
-              for( let i = 0; i < sims; i++ ) {
-  
-                  const { keptRolls: keptRolls, unkeptRolls: unkeptRolls, total: total } = rollAndKeep( roll, keep );
-  
-                  simulatedTotal += total;
-  
-              }
-  
-              await interaction.editReply( 'Rolled **' + roll + '**k**' + keep + '** *' + sims + '* times. Average result: **' + ( simulatedTotal / sims ) + '**' );
-          }
-      }
-  };
+            await interaction.deferReply();
+
+            let simulatedTotal = 0;
+
+            for( let i = 0; i < sims; i++ ) {
+
+                const { keptRolls: keptRolls, unkeptRolls: unkeptRolls, total: total } = rollAndKeep( roll, keep, emphasis, expanded );
+
+                simulatedTotal += ( total + add );
+
+            }
+
+            let output = 'Rolled **' + roll + '**k**' + keep + '**';
+              
+            if( add > 0 ) {
+                output += '+**' + add + '**';
+            }
+
+            await interaction.editReply( output + ' \u00D7 *' + sims + '*.  Average result: **' + ( simulatedTotal / sims ) + '**' );
+        }
+    }
+};
 
 
-  function parseInput( input ) {
-     const regex = /^(\d+)k(\d+)(?:\s(\d+))?$/;
-     const match = input.match(regex);
-     
-     if ( match ) {
-         const X = parseInt( match[ 1 ] );
-         const Y = parseInt( match[ 2 ] );
-         const Z = match[ 3 ] ? parseInt( match[ 3 ] ) : null;
-         
-         if ( !isNaN( X ) && !isNaN ( Y ) && ( Z === null || !isNaN( Z ) ) ) {
-             return { X, Y, Z };
-         } else {
-             console.log( 'X, Y, and Z must be numeric.' );
-         }
-     } else {
-         console.log( 'Invalid format. Please use the format "XkY" or "XkY Z"' );
-     }
- }
+function parseInput( input ) {
+    const regex = /^(\d+)k(\d+)(?:\+(\d+))?(?:\s(\d+))?(?:\s(e))?(?:\s(x))?$/;
+    const match = input.match(regex);
+
+    if (match) {
+        const X = parseInt(match[1]);
+        const Y = parseInt(match[2]);
+        const Q = match[3] ? parseInt(match[3]) : null;
+        const Z = match[4] ? parseInt(match[4]) : null;
+        const e = match[5] ? true : false;
+        const x = match[6] ? true : false;
+
+        if (!isNaN(X) && !isNaN(Y) && (Q === null || !isNaN(Q)) && (Z === null || !isNaN(Z))) {
+            return { X, Y, Q, Z, e, x };
+        } else {
+            console.log( 'X, Y, Q, and Z must be numeric.' );
+        }
+    } else {
+        console.log( 'Invalid format. Please use the format "XkY", "XkY+Q", "XkY Z", or "XkY+Q Z"' );
+    }
+}
+  
  
- 
- 
- function rollAndKeep( rolled, keep ) {
+ function rollAndKeep( rolled, keep, emphasis = false, expanded = false ) {
  
      const rolls = [];
  
      for (let i = 0; i < rolled; i++) {    
-         rolls.push( rollDie() );
+         rolls.push( rollDie( emphasis, expanded ) );
      }
  
      rolls.sort((a, b) => b - a);
@@ -113,17 +127,26 @@ module.exports = {
  
  
  
- function rollDie () {
-     let total = roll = getRandomNumber( 1, 10 ); // Roll a d10
+function rollDie ( emphasis = false, expanded = false ) {
+    let total = roll = getRandomNumber( 1, 10 ); // Roll a d10
  
-     // Explode on a 10
-     while (roll === 10) {
-          roll = getRandomNumber( 1, 10 ); // Roll again
-          total += roll; // Add the exploded roll to the total
-     }
+    let explode = 10;
+
+    if( expanded ) {
+        explode = 9;
+    }
+
+    if( roll == 1 && emphasis ) {
+        roll = getRandomNumber( 1, 10 );
+    }
+
+    while (roll >= explode ) {
+        roll = getRandomNumber( 1, 10 ); // Roll again
+        total += roll; // Add the exploded roll to the total
+    }
  
-     return total;
- }
+    return total;
+}
  
  
  
