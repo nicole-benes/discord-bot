cbdf66d0cc539f4127bbfc428ab3dd21b040ad6b
Nicole Benes <110053946+nicole-benes@users.noreply.github.com>
2024-04-27 18:01:10 -0400

    Working on deal [Making hand a class]

--- commands/utility/deal.js
+++ commands/utility/deal.js
@@ -23,113 +23,81 @@ module.exports = {
           const row = new ActionRowBuilder()
 			.addComponents( deal, cancel );               
 
-          const response = await interaction.reply({
-			content: `Do you want to draw a card?`,
-			components: [ row ],
-          });
-
-          const collector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 3_600_000 });
-
           let hand = [];
           let deck = new standardDeck();
 
+          // We're always going to draw two cards to start at least 
+          hand.push( deck.draw() );
+          hand.push( deck.draw() );
 
-//          const ratio =  0.63166847;
+/*
+          // This is the ratio of the pngs
           const ratio =  0.68870523;
 
+          // Set up our canvas size
           const height = 300;
           const width = height * ratio;
           const offset = 5;
           
+          // Create the actual canvas with room for padding between the cards
           const canvas = Canvas.createCanvas( ( 6 * width ) + ( 8 * offset ), ( height * 2 ) + ( 8 * offset ) );
 		const context = canvas.getContext( '2d' );
-
-          const cardOne = await Canvas.loadImage( './cards-png/king_of_hearts2.png' );
-          const cardTwo = await Canvas.loadImage( './cards-png/3_of_clubs.png' );
 
           // Loop through each of the cards currently in the hand
           for( let i = 0; i < hand.length; i++ ) {
 
-          }
+               // Get the filename for this card
+               const cardImage = await Canvas.loadImage( './cards-png/' + deck.getFilename( hand[ i] ) );
 
-/*
-//          const background = await Canvas.loadImage( './cards-png/king_of_hearts2.png' );
-          context.drawImage(cardOne, offset, offset, width, height + offset);
-          context.drawImage(cardTwo, width + offset, offset, width, height + offset  );
-          context.drawImage(cardOne, ( 2 * width ) + ( 2 * offset ), offset, width, height + offset );
-*/
+               // We need to know which column we working with
+               let columnCount = i;
 
-          for( let i = 0; i < 12; i++ ) {
-
-               let rowCount = i;
+               // Assume we're working on the first row
                let y1 = offset;
                let y2 = offset + height;
 
-               if( rowCount > 5 ) {
-                    rowCount = i - 6;
+               // If we're on card 6 and up, we need another row
+               if( columnCount > 5 ) {
+
+                    // Start over at column 0
+                    columnCount = i - 6;
+
+                    // Update y values
                     y1 = ( offset * 3 ) + height;
                     y2 = ( offset * 3 ) + height;
                }
 
-               let x1 = ( rowCount + 1 ) * offset + ( rowCount * width );
+               // Figure out the top left corner of this card
+               let x1 = ( columnCount + 1 ) * offset + ( columnCount * width );
 
-               context.drawImage(cardOne, x1, y1, width, y2 );
-
-/*
-               let topLeftX = ( ( i + 1 ) * offset ) + ( i * width );
-
-               let topLeftY = offset;
-
-               if( i > 5 ) {
-                    topLeftY = ( offset * 2 ) + height;
-
-               }
-//               let bottomRightX = ( ( i + 1 ) * offset ) + ( ( i + 1 ) * width );
-//               let bottomRightY = height;
-
-//               console.log( topLeftX + ',' + topLeftY + ' -> ' + bottomRightX + ',' + bottomRightY );
-
-               context.drawImage(cardOne, topLeftX, topLeftY, width, ( offset + height ) );
-*/
+               // Add the current card to our image in the right place
+               context.drawImage( cardImage, x1, y1, width, y2 );
           }
 
-          const attachment = new AttachmentBuilder(await canvas.encode( 'png' ), { name: 'card.png' });
+*/
+          // Render the finalized image of our current hand
+          let renderedCards = new AttachmentBuilder( await canvas.encode( 'png' ), { name: 'cards.png' } );
+          
+          const response = await interaction.reply({
+			content: 'Cards Drawn: **' + hand.length + '**' + '. Do you want to draw another card?',
+               files: [ renderedCards ],
+			components: [ row ],
+          });
 
-          let files = [];
+     
+          const collector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 3_600_000 });
           
           collector.on('collect', async i => {
                const selection = i.customId;
-
                if( selection == 'deal' ) {
-                    presses += 1;
                     hand.push( deck.draw() );
-                    files.push( attachment );
-                    
-                    for( let i = 0; i < hand.length; i++ ) {
-                         switch( hand[ i ].suit ) {
-                              case 'h':
-                                   break;
-                              case 'd':
-                                   break;
-                              case 's':
-                                   break;
-                              case 'c':
-                                   break;
-                              default:
-                                   break;
-                         }
-                    }
-                    await i.update({
-                         files: [ attachment ],
 
-                    })
-/*
                     await i.update({
-                         content: '# ' + renderedHand,
-//                         content: 'Hand (' + presses + '): ' + renderedHand,
-                         components: [row],
-                    });
-*/
+                         content: 'Cards Drawn: **' + hand.length + '**' + '. Do you want to draw another card?',
+                         files: [ renderedCards ],
+                         components: [ row ],
+                    })
+
                } else {
                     const disabledDeal = new ButtonBuilder()
                          .setCustomId( 'disabledDeal' )
@@ -146,11 +114,68 @@ module.exports = {
                     const disabledRow = new ActionRowBuilder()
                          .addComponents( disabledDeal, disabledCancel );
 
-                    await i.update({ content: 'Cards Dawn: **' + presses + '**', components: [ disabledRow ] });
+                    await i.update({ 
+                         content: 'Cards Dawn: **' + hand.length + '**',
+                         files: [ renderedCards ],
+                         components: [ disabledRow ],
+                    });
                }
           });          
      }
 };
+
+class pokerHand {
+     constructor() {
+          this.deck = new standardDeck();
+     }
+
+     renderHand() {
+          // This is the ratio of the pngs
+          const ratio =  0.68870523;
+
+          // Set up our canvas size
+          const height = 300;
+          const width = height * ratio;
+          const offset = 5;
+          
+          // Create the actual canvas with room for padding between the cards
+          const canvas = Canvas.createCanvas( ( 6 * width ) + ( 8 * offset ), ( height * 2 ) + ( 8 * offset ) );
+          const context = canvas.getContext( '2d' );
+
+          // Loop through each of the cards currently in the hand
+          for( let i = 0; i < hand.length; i++ ) {
+
+               // Get the filename for this card
+               const cardImage = await Canvas.loadImage( './cards-png/' + deck.getFilename( hand[ i] ) );
+
+               // We need to know which column we working with
+               let columnCount = i;
+
+               // Assume we're working on the first row
+               let y1 = offset;
+               let y2 = offset + height;
+
+               // If we're on card 6 and up, we need another row
+               if( columnCount > 5 ) {
+
+                    // Start over at column 0
+                    columnCount = i - 6;
+
+                    // Update y values
+                    y1 = ( offset * 3 ) + height;
+                    y2 = ( offset * 3 ) + height;
+               }
+
+               // Figure out the top left corner of this card
+               let x1 = ( columnCount + 1 ) * offset + ( columnCount * width );
+
+               // Add the current card to our image in the right place
+               context.drawImage( cardImage, x1, y1, width, y2 );
+          }
+
+          return canvas.encode( 'png' );
+     }
+}
 
 class standardDeck {
      constructor() {
@@ -173,7 +198,7 @@ class standardDeck {
           this.deck.push( { suit: 'joker', rank: 'black' } );
      }
 
-     draw() {
+     drawCard() {
           if( this.deck.length > 0 ) {
                const draw = this.getRandomNumber( 0 * this.deck.length );
 
@@ -182,8 +207,12 @@ class standardDeck {
 
           return false;
      }
+
+     getFilename( card ) {
+          return '3_of_clubs.png';
+     }
      
-     getRandomNumber(min, max) {
+     getRandomNumber( min, max ) {
           const range = max - min + 1;
           const buffer = crypto.randomBytes(4);
           const randomNumber = buffer.readUInt32LE(0) % range + min;
