9215312a18d18e7da28686d4ec7602c528c34ffd
Nicole Benes <nicolembenes@gmail.com>
2024-11-27 19:32:42 -0500

    Working DL support

--- commands/utility/d.js
+++ commands/utility/d.js
@@ -1,21 +1,14 @@
-const { SlashCommandBuilder } = require('discord.js');
-const crypto = require('crypto');
+const { SlashCommandBuilder } = require( 'discord.js' );
 
 module.exports = {
      cooldown: 5,
      category: 'utility',
-     data: new SlashCommandBuilder()
+	data: new SlashCommandBuilder()
 		.setName('dl')
-		.setDescription('Roll a number of exploding dice and keep the best.')
-		.addStringOption(option =>
-			option
-				.setName( 'options' )
-                    .setRequired( true )
-				.setDescription( 'TBD' ) ),
-     async execute(interaction) {
-  
-          const Options = interaction.options.getString( 'options' );
-
-          await interaction.reply( Options );   
-     },
-}
+		.setDescription('TBD'),
+	async execute(interaction) {
+		// interaction.user is the object representing the User who ran the command
+		// interaction.member is the GuildMember object, which represents the user in the specific guild
+		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+	},
+};
